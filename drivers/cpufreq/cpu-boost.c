1ad62b63f124b07952de06a5f7c41de0dbbc3d7d
andip71 <andreasp@gmx.de>
2015-09-15 06:46:48 +0200

    cpu-boost: Implement touch boost configuration driver

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -26,6 +26,12 @@
 #include <linux/input.h>
 #include <linux/time.h>
 
+#ifdef CONFIG_TOUCHBOOST_CONTROL
+#include <linux/export.h>
+unsigned int input_boost_status = 1;
+unsigned int input_boost_freq = 1344000;
+#endif
+
 struct cpu_sync {
 	struct task_struct *thread;
 	wait_queue_head_t sync_wq;
@@ -56,7 +62,7 @@ module_param(sysctl_thermal_aware_scheduling, uint, 0644);
 static unsigned int sync_threshold;
 module_param(sync_threshold, uint, 0644);
 
-static bool input_boost_enabled;
+bool input_boost_enabled;
 
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
@@ -122,6 +128,14 @@ check_enable:
 
 	return 0;
 }
+
+#ifdef CONFIG_TOUCHBOOST_CONTROL
+void set_touchboost_frequency(void)
+{
+	per_cpu(sync_info, 0).input_boost_freq = input_boost_freq;
+}
+EXPORT_SYMBOL(set_touchboost_frequency);
+#endif
 
 static int get_input_boost_freq(char *buf, const struct kernel_param *kp)
 {
@@ -390,6 +404,12 @@ static void cpuboost_input_event(struct input_handle *handle,
 		unsigned int type, unsigned int code, int value)
 {
 	u64 now;
+
+#ifdef CONFIG_TOUCHBOOST_CONTROL
+	// if touch boost (input boost) is switched off, do nothing
+	if (!input_boost_status)
+		return;
+#endif
 
 	if (!input_boost_enabled)
 		return;
