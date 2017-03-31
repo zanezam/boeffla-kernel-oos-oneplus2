8b891f33d507093848e9e866c4b0d77e7bfd2559
andip71 <andreasp@gmx.de>
2017-03-31 20:47:09 +0800

    misc: fpc1020: Fix s1302_is_keypad_stopped() logic for home key
    All credits to Sultanxda <sultanxda@gmail.com>
    
    Ported to OnePlus2 by Wang Han (aka aviraxp)
    
    When the user's finger is on the touchscreen, don't process any home key
    button presses, as an unintended button press will cause poor UX.
    
    Ignore home key presses when the Synaptics s1302 driver is ignoring keypad
    presses.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -332,7 +332,7 @@ static ssize_t report_home_set(struct device *dev,
 	else if (!strncmp(buf, "up", strlen("up")))
 	{
 #ifdef VENDOR_EDIT //WayneChang, 2015/12/02, add for key to abs, simulate key in abs through virtual key system
-		if(ignore_keypad){
+		if(virtual_key_enable){
                 key_home_pressed = false;
 		}else{
 			input_report_key(fpc1020->input_dev,
